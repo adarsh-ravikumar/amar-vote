96056a2c1ec84442d2273a12aab0ea356c84392e
adarsh_ravikumar <azracstudios@gmail.com>
2025-02-03 05:28:00 +0530

    impl: add candidate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,23 +5,3 @@
 </script>
 
 <slot></slot>
-
-<p class="credits">Made with ❤️ by <span>Adarsh Ravikumar</span> - Batch of 2025</p>
-
-<style lang="scss">
-	@use '../styles/vars' as *;
-
-	.credits {
-		position: fixed;
-		bottom: 0;
-		right: 0;
-		font-size: 1.1rem;
-		font-weight: 500;
-		padding: 0.2rem;
-
-		span {
-			color: $latte-blue;
-			font-weight: 600;
-		}
-	}
-</style>
