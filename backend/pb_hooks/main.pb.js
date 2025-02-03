a77f24ab5d244d4ed296deaf23b9fa22c21dac5c
adarsh_ravikumar <azracstudios@gmail.com>
2025-02-03 11:07:10 +0530

    chore: update pb

--- backend/pb_hooks/main.pb.js
+++
@@ -1,5 +0,0 @@
-/// <reference  path="../../pb_data/types.d.ts" />
-onBootstrap(function (e) {
-    e.next();
-    console.log('Hello World!');
-});
--- backend/pb_hooks/src/main.pb.ts
+++
@@ -1,6 +0,0 @@
-/// <reference  path="../../pb_data/types.d.ts" />
-
-onBootstrap((e) => {
-	e.next();
-	console.log('Hello World!');
-});
